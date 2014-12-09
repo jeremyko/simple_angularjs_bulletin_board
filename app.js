ed31a7244e3271f3728078c1a5a18ef0bdc0d6de
jeremyko <jeremyko69@gmail.com>
2014-12-09 12:24:44 +0900

    2nd commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
-app.set('views_NotInUse', path.join(__dirname, 'views_NotInUse'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
